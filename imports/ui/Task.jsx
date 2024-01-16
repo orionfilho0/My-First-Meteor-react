b623ad2645ef330510681468884d33120f8947f5
orionfilho0 <orionfilho3@gmail.com>
2024-01-16 10:52:56 -0300

    implementando publicacoes

--- imports/ui/Task.jsx
+++ imports/ui/Task.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 
 export const Task = ({ task, onCheckboxClick, onDeleteClick }) => {
   return (
--- server/main.js
+++ server/main.js
@@ -1,9 +1,30 @@
 import { Meteor } from 'meteor/meteor';
-import { TasksCollection } from '/imports/api/TasksCollection';
+import { Accounts } from 'meteor/accounts-base';
+import { TasksCollection } from '/imports/db/TasksCollection';
+import { ServiceConfiguration } from 'meteor/service-configuration';
+import '/imports/api/tasksMethods';
+import '/imports/api/tasksPublications';
 
-const insertTask = taskText => TasksCollection.insert({ text: taskText });
+const insertTask = (taskText, user) =>
+  TasksCollection.insert({
+    text: taskText,
+    userId: user._id,
+    createdAt: new Date(),
+  });
+
+const SEED_USERNAME = 'meteorite';
+const SEED_PASSWORD = 'password';
 
 Meteor.startup(() => {
+  if (!Accounts.findUserByUsername(SEED_USERNAME)) {
+    Accounts.createUser({
+      username: SEED_USERNAME,
+      password: SEED_PASSWORD,
+    });
+  }
+
+  const user = Accounts.findUserByUsername(SEED_USERNAME);
+
   if (TasksCollection.find().count() === 0) {
     [
       'First Task',
@@ -12,7 +33,18 @@ Meteor.startup(() => {
       'Fourth Task',
       'Fifth Task',
       'Sixth Task',
-      'Seventh TaskTESTE'
-    ].forEach(insertTask)
+      'Seventh Task',
+    ].forEach(taskText => insertTask(taskText, user));
   }
 });
+
+ServiceConfiguration.configurations.upsert(
+  { service: 'github' },
+  {
+    $set: {
+      loginStyle: 'popup',
+      clientId: '3be1e05a5f88502b4384', // insert your clientId here
+      secret: '5b67d36f3d3a2a0cc235c56135e9eba51d0bc63c', // insert your secret here
+    },
+  }
+);
